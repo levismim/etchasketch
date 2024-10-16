a33a663dedc2fbaed52170ce343f7ad4348015a6
levismim <86032562+levismim@users.noreply.github.com>
2024-10-16 14:11:04 -0500

    create function for button
    prompts user to choose a number between 1-100

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const container = document.getElementById("container")
-const divs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-const changeSize = document.getElementById("change-size")
+let divs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+const changeAmount = document.getElementById("change-amount")
+
+changeAmount.addEventListener("click", function() {
+    let amount = prompt("How many squares per side? Cannot be more than 100.")
+    if (amount > 100 || amount < 0) {
+        alert("invalid number.")
+    } else if (amount <= 100) {
+        return amount;
+    }
+})  
 
 for (let i = 0; i <= 16; i++) {
-    divs.forEach(function(e) {
+    divs.forEach(function() {
         let div = document.createElement("div")
         div.className = "square"
         container.appendChild(div)
-        div.addEventListener("mouseover", function(e) {div.style.backgroundColor = "lightblue"})
+        div.addEventListener("mouseover", function() 
+            {div.style.backgroundColor = "lightblue"})
     })
 }
 
