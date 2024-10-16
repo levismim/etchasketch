a086abe1fe2a2d84f6c225e2d43d5de04dc33d43
levismim <86032562+levismim@users.noreply.github.com>
2024-10-16 13:45:52 -0500

    create 16x16 grid, add button

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const container = document.getElementById("container")
 const divs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-divs.forEach(function(e) {
-    let div = document.createElement("div")
-    div.className = "square"
-    container.appendChild(div)
-})
+const changeSize = document.getElementById("change-size")
+
+for (let i = 0; i <= 16; i++) {
+    divs.forEach(function(e) {
+        let div = document.createElement("div")
+        div.className = "square"
+        container.appendChild(div)
+        div.addEventListener("mouseover", function(e) {div.style.backgroundColor = "lightblue"})
+    })
+}
+
