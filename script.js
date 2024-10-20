5c39388e26ff9a9d843c41eb109193460b5c74b9
levismim <86032562+levismim@users.noreply.github.com>
2024-10-20 08:02:45 -0500

    finish project

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
 const container = document.getElementById("container")
-let divs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 const changeAmount = document.getElementById("change-amount")
+
+function makeGrid() {
+for (i = 0; i < 16; i++) {
+        const column = document.createElement("div")
+        column.className = "column"
+        container.appendChild(column)
+    
+
+for (j = 0; j < 16; j++) {
+        const row = document.createElement("div")
+        row.className = "row"
+        column.appendChild(row)
+        row.textContent = ''
+        row.addEventListener("mouseover", function() 
+            {row.style.backgroundColor = "black"})
+}}
+}
+makeGrid()
 
 changeAmount.addEventListener("click", function() {
     let amount = prompt("How many squares per side? Cannot be more than 100.")
+    let num = parseInt(amount)
+  
     if (amount > 100 || amount < 0) {
         alert("invalid number.")
     } else if (amount <= 100) {
-        return amount;
+
+        function newGrid() {
+            document.getElementById('container').innerHTML = '';
+            for (i = 0; i < num; i++) {
+                    const column = document.createElement("div")
+                    column.className = "column"
+                    container.appendChild(column)
+                    column.textContent = ''
+            
+            for (j = 0; j < num; j++) {
+                    const row = document.createElement("div")
+                    row.className = "row"
+                    column.appendChild(row)
+                    row.textContent = ''
+                    row.addEventListener("mouseover", function() 
+                        {row.style.backgroundColor = "black"})
+            }}
+            }
+            newGrid()
     }
 })  
-
-for (let i = 0; i <= 16; i++) {
-    divs.forEach(function() {
-        let div = document.createElement("div")
-        div.className = "square"
-        container.appendChild(div)
-        div.addEventListener("mouseover", function() 
-            {div.style.backgroundColor = "lightblue"})
-    })
-}
 
